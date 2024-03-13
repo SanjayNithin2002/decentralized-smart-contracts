2c1ab419bb58e48ded904649b4c1c0821ed18e8d
Sanjay Nithin <sanjayfornewsletter@gmail.com>
2024-03-14 05:06:45 +0530

    data owner contract updated

--- DataOwnerContract.sol
+++ DataOwnerContract.sol
@@ -12,23 +12,29 @@ contract DataOwnerContract {
 
     mapping(string => DataOwner) public dataOwners;
     string[] public dataOwnerIds;
-    mapping(string => bool) public emailExists;
 
     event DataOwnerCreated(string id, string name, string email, string department, string role);
 
     function createDataOwner(string memory _id, string memory _name, string memory _email, string memory _password, string memory _department, string memory _role) public {
-        require(!emailExists[_email], "Email already exists");
+        require(bytes(dataOwners[_id].id).length == 0, "Data owner already exists");
+        require(!_isEmailTaken(_email), "Email already exists");
+
         dataOwners[_id] = DataOwner(_id, _name, _email, _password, _department, _role);
         dataOwnerIds.push(_id);
-        emailExists[_email] = true;
+        
         emit DataOwnerCreated(_id, _name, _email, _department, _role);
     }
 
-    function doesEmailExist(string memory _email) public view returns (bool) {
-        return emailExists[_email];
+    function _isEmailTaken(string memory _email) internal view returns (bool) {
+        for (uint i = 0; i < dataOwnerIds.length; i++) {
+            if (keccak256(bytes(dataOwners[dataOwnerIds[i]].email)) == keccak256(bytes(_email))) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    function getDataOwnerById(string memory _id) public view returns (DataOwner memory) {
+    function getById(string memory _id) public view returns (DataOwner memory) {
         require(bytes(dataOwners[_id].id).length != 0, "Data owner does not exist");
         return dataOwners[_id];
     }
